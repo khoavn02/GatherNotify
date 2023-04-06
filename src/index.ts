48f6aa58472a828bc84684bdfe2fa1f96bc88581
vnkhoa02 <khoacaro96@gmail.com>
2023-04-06 17:14:27 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,70 @@
+import * as dotenv from "dotenv";
 import AWS from "aws-sdk";
+
+dotenv.config();
 
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-const AWS_REGION = process.env.AWS_ACCESS_KEY_ID;
+const AWS_REGION = process.env.AWS_REGION;
 
-const credentials = new AWS.Credentials({
-  accessKeyId: AWS_ACCESS_KEY_ID as string,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY as string,
+AWS.config.update({
+  region: AWS_REGION,
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
 });
-const sns = new AWS.SNS({ credentials: credentials, region: AWS_REGION });
 
-async function createTopicIfNotFound(topicName: string): Promise<string> {
-  try {
-    const data = await sns.listTopics().promise();
-    const topic = data.Topics?.find((t) =>
-      t.TopicArn?.endsWith(`:${topicName}`)
-    );
-
-    if (topic) {
-      return topic.TopicArn!;
-    } else {
-      const createTopicResponse = await sns
-        .createTopic({ Name: topicName })
+function checkIfTopicExists(AWS: any, topicName: string) {
+  return new Promise((resolve, reject) => {
+    try {
+      const listTopics = new AWS.SNS({ apiVersion: "2010-03-31" })
+        .listTopics({})
         .promise();
-      return createTopicResponse.TopicArn!;
-    }
-  } catch (err) {
-    throw new Error(JSON.stringify(err));
-  }
-}
-
-export async function notify(type: string, data: unknown) {
-  const topic = await createTopicIfNotFound(type);
-  const params = {
-    Message: JSON.stringify(data),
-    TopicArn: topic,
-  };
-
-  sns.publish(params, (err, data) => {
-    if (err) {
-      throw new Error(JSON.stringify(err));
-    } else {
-      console.log("Message sent:", data.MessageId);
+      listTopics
+        .then((data: any) => {
+          if (data.Topics.includes(topicName)) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        })
+        .catch((err: any) => {
+          throw err;
+        });
+    } catch (e) {
+      reject(e);
     }
   });
 }
+
+function createTopic(AWS: any, topicName: string) {
+  return new Promise((resolve, reject) => {
+    try {
+      const createTopic = new AWS.SNS({ apiVersion: "2010-03-31" })
+        .createTopic({
+          Name: topicName,
+        })
+        .promise();
+      createTopic
+        .then((data: any) => {
+          console.log(`Created Topic - ${topicName}`);
+          console.log("data", data);
+          resolve(data.TopicArn);
+        })
+        .catch((err: any) => {
+          throw err;
+        });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+export async function notify(type: string, data: unknown) {
+  const ifTopicExists = await checkIfTopicExists(AWS, "ON_POST_CREATED");
+
+  if (!ifTopicExists) {
+    await createTopic(AWS, "ON_POST_CREATED");
+  }
+}
+
+notify("cat", "meo meo");
