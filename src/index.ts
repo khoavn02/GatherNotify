e979268ae25bf39b7f827ea33a3b999256b25382
vnkhoa02 <khoacaro96@gmail.com>
2023-04-07 17:06:00 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -13,58 +13,100 @@ AWS.config.update({
   secretAccessKey: AWS_SECRET_ACCESS_KEY,
 });
 
-function checkIfTopicExists(AWS: any, topicName: string) {
-  return new Promise((resolve, reject) => {
-    try {
-      const listTopics = new AWS.SNS({ apiVersion: "2010-03-31" })
-        .listTopics({})
-        .promise();
-      listTopics
-        .then((data: any) => {
-          if (data.Topics.includes(topicName)) {
-            resolve(true);
-          } else {
-            resolve(false);
-          }
-        })
-        .catch((err: any) => {
-          throw err;
-        });
-    } catch (e) {
-      reject(e);
-    }
-  });
-}
+const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
 
-function createTopic(AWS: any, topicName: string) {
-  return new Promise((resolve, reject) => {
-    try {
-      const createTopic = new AWS.SNS({ apiVersion: "2010-03-31" })
-        .createTopic({
-          Name: topicName,
-        })
-        .promise();
-      createTopic
-        .then((data: any) => {
-          console.log(`Created Topic - ${topicName}`);
-          console.log("data", data);
-          resolve(data.TopicArn);
-        })
-        .catch((err: any) => {
-          throw err;
-        });
-    } catch (e) {
-      reject(e);
-    }
-  });
-}
-
-export async function notify(type: string, data: unknown) {
-  const ifTopicExists = await checkIfTopicExists(AWS, "ON_POST_CREATED");
-
-  if (!ifTopicExists) {
-    await createTopic(AWS, "ON_POST_CREATED");
+/**
+* Checks if an SNS topic with the specified name exists and returns its Amazon Resource Name (ARN).
+  @param topicName The name of the SNS topic to be checked.
+  @returns A promise that resolves with the ARN of the SNS topic if it exists, or null if it doesn't.
+  @throws If the topic name is invalid or the list of topics fails.
+*/
+const checkIfTopicExists = async (
+  topicName: string
+): Promise<string | null> => {
+  if (typeof topicName !== "string" || topicName.trim().length === 0) {
+    throw new Error("Invalid topic name");
   }
+
+  const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
+  const topics = await sns.listTopics().promise();
+
+  if (!topics.Topics || !Array.isArray(topics.Topics)) {
+    throw new Error("Failed to list topics");
+  }
+
+  const topic = topics.Topics.find((topic) =>
+    topic.TopicArn?.includes(topicName)
+  );
+
+  return topic?.TopicArn || null;
+};
+
+/**
+* Creates an SNS topic with the specified name.
+  @param topicName The name of the SNS topic to be created.
+  @returns A promise that resolves with the Amazon Resource Name (ARN) of the created SNS topic.
+  @throws If the topic name is invalid or the creation of the topic fails.
+*/
+const createTopic = async (topicName: string): Promise<string> => {
+  if (typeof topicName !== "string" || topicName.trim().length === 0) {
+    throw new Error("Invalid topic name");
+  }
+
+  const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
+  const data = await sns.createTopic({ Name: topicName }).promise();
+
+  if (!data.TopicArn) {
+    throw new Error("Failed to create topic");
+  }
+
+  return data.TopicArn;
+};
+
+/**
+* Publishes a message to an SNS topic.
+  @param data The data to be sent as a string.
+  @param topicArn The Amazon Resource Name (ARN) of the SNS topic to which the message should be published.
+  @param subject The subject of the message (optional).
+  @returns A promise that resolves with the response from SNS.
+*/
+const publishMessage = async (
+  data: string,
+  topicArn: string,
+  subject?: string
+) => {
+  const params = {
+    Message: data,
+    Subject: subject,
+    TopicArn: topicArn,
+  };
+
+  try {
+    return await sns.publish(params).promise();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+/**
+* Sends a message to an SNS topic.
+  @param type The type of message, e.g. "Email".
+  @param data The data to be sent.
+  @param subject The subject of the message (optional).
+  @returns Promise that resolves with the response from SNS.
+*/
+export async function notify(type: string, data: unknown, subject?: string) {
+  let topicArn = await checkIfTopicExists(type);
+
+  if (!topicArn) {
+    topicArn = await createTopic(type);
+  }
+
+  return await publishMessage(JSON.stringify(data), topicArn, subject);
 }
 
-notify("cat", "meo meo");
+notify(
+  "Notification",
+  { message: "Den gio an com", time: "7h" },
+  "Thong bao moi"
+);
