432519715a887c634a8ab88cdb22fb5ad950c1ae
vnkhoa02 <khoacaro96@gmail.com>
2023-04-13 16:57:47 +0700

    update code

--- src/sns/index.ts
+++ src/sns/index.ts
@@ -1,7 +1,4 @@
-import * as dotenv from "dotenv";
 import AWS from "aws-sdk";
-
-dotenv.config();
 
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
